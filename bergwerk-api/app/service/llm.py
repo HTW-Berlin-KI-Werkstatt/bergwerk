46201762ec57c65b13685ab9c7d7a36636cb1d72
Florian Dewes <dewes@htw-berlin.de>
2025-06-30 15:59:14 +0200

    Refactor llm_training_data function to streamline model loading and training content generation, removing unnecessary print statements and comments.

--- bergwerk-api/app/service/llm.py
+++ bergwerk-api/app/service/llm.py
@@ -20,11 +20,7 @@ def llm_training_data():
 
     for model in model_list:
 
-        print(model)
-
         r = data_ollama.load_model(model)
-        print(r.text)
-        print(f"Try to load model {model}")
 
         for p in pages:
 
@@ -36,42 +32,34 @@ def llm_training_data():
 
             r = data_ollama.query_llm(prompt, model)
             full_page = data_wiki.get_entire_page(p)
-            full_page += f"\n= Deutsche Trainingsdaten vom {model}=\n" + "\n==== Prompt ====\n" + "<pre>\n" + prompt + "</pre>""\n==== Raw Model Output ====\n" + "<pre>\n" + r + "</pre>"
+            full_page += f"\n= German training data: {model}=\n" + "\n==== Prompt ====\n" + "<pre>\n" + prompt + "</pre>""\n==== Raw Model Output ====\n" + "<pre>\n" + r + "</pre>"
+
+            data_wiki.create_or_update_page(p, full_page)
+
+
+    model_list = service_config.get_configitem("English_llm_models_training_list").value
+    instruction = service_config.get_configitem("English_llm_models_training_instruction").value
+    print(model_list, instruction)
+
+    for model in model_list:
+
+        r = data_ollama.load_model(model)
+
+        for p in pages:
+
+            print(f"Generating training content with {model} for page {p}")
+
+            en_text = service_wiki.get_page(page=p, language="Deutsch").text
+
+            prompt = instruction + "\n" + en_text 
+
+            r = data_ollama.query_llm(prompt, model)
+            full_page = data_wiki.get_entire_page(p)
+            full_page += f"\n= English training data: {model}=\n" + "\n==== Prompt ====\n" + "<pre>\n" + prompt + "</pre>""\n==== Raw Model Output ====\n" + "<pre>\n" + r + "</pre>"
 
             data_wiki.create_or_update_page(p, full_page)
                 
 
-
-    # model_list = service_config.get_configitem("English_llm_models_training_list")
-    # print(model_list)
-    # model_list = [m.strip() for m in model_list]    
-    # instruction = service_config.get_configitem("English_llm_models_training_instruction").value
-
-    # for model in model_list:
-
-    #     data_ollama.load_model(model)
-    #     print(f"Try to load model {model}")
-
-    #     for p in pages:
-
-
-    #         try:
-
-    #             print(f"Generating training content with {model} for page {p}")
-
-    #             en_text = service_wiki.get_page(page=p, language="English").text
-
-    #             prompt = instruction + "\n" + en_text 
-
-    #             r = data_ollama.query_llm(prompt, model)
-    #             full_page = data_wiki.get_entire_page(p)
-    #             full_page += f"\n= English Training Questions from {model}=\n" + "\n==== Prompt ====\n" + "<pre>\n" + prompt + "</pre>""\n==== Raw Model Output ====\n" + "<pre>\n" + r + "</pre>"
-
-    #             data_wiki.create_or_update_page(p, full_page)
-                
-    #         except Exception as e:
-    #             print(e)
-    #             continue
 
 
 
